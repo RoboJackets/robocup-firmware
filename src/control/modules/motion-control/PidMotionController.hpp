294d2962444c01997078dbf1af8cacc549578e4e
Jeremy Feltracco <jpfeltracco@gatech.edu>
2018-06-10 02:24:00 -0400

    Add rotation estimate to firmware controller.
    This estimate is derived from a weighted combination of encoder to
    body estimates and gyro measurements. A rotation hold pid controller
    has been added to test the measurement.
    
    This work is attempting to validate the potential of performing position
    level control in firmware to benefit from the reduced sensor latency,
    a higher update rate, and faster motor control rate.

--- src/control/modules/motion-control/PidMotionController.hpp
+++ src/control/modules/motion-control/PidMotionController.hpp
@@ -3,6 +3,8 @@
 #include <array>
 #include <rc-fshare/pid.hpp>
 #include <rc-fshare/robot_model.hpp>
+
+#include "MPU6050.h"
 #include "FPGA.hpp"
 
 /**
@@ -10,7 +12,47 @@
  */
 class PidMotionController {
 public:
-    PidMotionController() { setPidValues(3.0, 0, 0, 50, 0); }
+    PidMotionController() : imu(MPU6050_DEFAULT_ADDRESS, RJ_I2C_SDA, RJ_I2C_SCL),
+        ax_offset(0), ay_offset(0), az_offset(0), gx_offset(0), gy_offset(0), gz_offset(0),
+        ax(0), ay(0), az(0), gx(0), gy(0), gz(0), rotation(0), angular_vel(0)
+    { 
+        setPidValues(3.0, 0, 0, 50, 0);
+        imu.initialize();
+
+        FILE *fp = fopen("/local/offsets.txt", "r");  // Open "out.txt" on the local file system for writing
+
+        int success = 0;
+        printf("opening gyro offsets file\r\n");
+        if (fp != nullptr) {
+            success = fscanf(fp, "%d %d %d %d %d %d", &ax_offset, &ay_offset, &az_offset,
+                                                      &gx_offset, &gy_offset, &gz_offset);
+            printf("fscanf done of gyro offsets\r\n");
+            fclose(fp);
+            printf("closed gyro offset file\r\n");
+        }
+
+        if (success == 6) {
+            printf("Successfully imported offsets from offsets.txt\r\n");
+        } else {
+            printf("Failed to import offsets from offsets.txt, defaulting to 0\r\n");
+        }
+
+        Thread::wait(2000);
+
+        imu.setFullScaleGyroRange(MPU6050_GYRO_FS_1000);
+        imu.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
+
+        imu.setXAccelOffset(ax_offset);
+        imu.setYAccelOffset(ay_offset);
+        imu.setZAccelOffset(az_offset);
+        imu.setXGyroOffset(gx_offset);
+        imu.setYGyroOffset(gy_offset);
+        imu.setZGyroOffset(gz_offset);
+
+        rotation_pid.kp = 5;
+        rotation_pid.ki = 0;
+        rotation_pid.kd = 0;
+    }
 
     void setPidValues(float p, float i, float d, unsigned int windup,
                       float derivAlpha) {
@@ -56,14 +98,38 @@ public:
                                float dt, Eigen::Vector4d* errors = nullptr,
                                Eigen::Vector4d* wheelVelsOut = nullptr,
                                Eigen::Vector4d* targetWheelVelsOut = nullptr) {
-        // convert encoder ticks to rad/s
-        Eigen::Vector4d wheelVels;
-        wheelVels << encoderDeltas[0], encoderDeltas[1], encoderDeltas[2],
-            encoderDeltas[3];
-        wheelVels *= 2.0 * M_PI / ENC_TICKS_PER_TURN / dt;
+        // convert sensor readings to mathematically valid values
+        imu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
-        Eigen::Vector4d targetWheelVels =
-            RobotModel::get().BotToWheel * _targetVel.cast<double>();
+        Eigen::Vector4d wheelVels;
+        wheelVels << encoderDeltas[0], encoderDeltas[1], encoderDeltas[2], encoderDeltas[3];
+        wheelVels *= 2.0 * M_PI / ENC_TICKS_PER_TURN / dt;
+        auto body_vels = RobotModel::get().WheelToBot * wheelVels;
+
+        // two redundent sensor measurements for rotation
+        float ang_vel_gyro = 32.8 * gz;
+        float ang_vel_enc = body_vels[2];
+
+        std::printf("%f %f\r\n", ang_vel_gyro, ang_vel_enc);
+
+        // perform sensor fusion
+        // the higher this is, the more gyro measurements are used instead of encoders
+        float sensor_fuse_ratio = 0.9;
+        float ang_vel_update = ang_vel_gyro * sensor_fuse_ratio
+                             + ang_vel_enc * sensor_fuse_ratio;
+
+        // perform state update based on fused value
+        float alpha = 0.8;
+        rotation = alpha * ang_vel_update + (1 - alpha) * angular_vel;
+
+        // rotation controller
+        float target_w = _targetVel[2];
+        target_w = rotation_pid.run(rotation); // rotation pid to do an angular hold
+        // correct target velocity to include rotation hold
+        _targetVel[2] += target_w;
+
+        // conversion to commanded wheel velocities
+        Eigen::Vector4d targetWheelVels = RobotModel::get().BotToWheel * _targetVel.cast<double>();
 
         if (targetWheelVelsOut) {
             *targetWheelVelsOut = targetWheelVels;
@@ -127,5 +193,17 @@ private:
     /// controllers for each wheel
     std::array<Pid, 4> _controllers{};
 
+    Pid rotation_pid;
+
     Eigen::Vector3f _targetVel{};
+
+    MPU6050 imu;
+
+    int ax_offset, ay_offset, az_offset,
+        gx_offset, gy_offset, gz_offset;
+    int16_t ax, ay, az,
+            gx, gy, gz;
+
+    float rotation; // state estimate for rotation
+    float angular_vel;
 };
