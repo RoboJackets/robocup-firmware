58eb9bbbcf4eddf0b19a3a5db179aabd512cf0fd
CollinAvidano <collin.avidano@gmail.com>
2018-12-29 20:14:51 -0500

    Still just working on the radio test

--- src/hw-test/test-new-radio.cpp
+++ src/hw-test/test-new-radio.cpp
@@ -1,5 +1,3 @@
-// #define __CM3_REV 0x200
-
 #include <mbed.h>
 #include <vector>
 #include "Logger.hpp"
@@ -19,7 +17,7 @@ const PinName RADIO_SPI_NCS = p20;
 const PinName RADIO_DATA_RDY = p21;
 const PinName RADIO_RST = p22;
 
-uint16_t readBuffer[128];
+uint16_t readBuffer[512];
 
 DigitalIn dataReady(RADIO_DATA_RDY);
 DigitalOut ssn(RADIO_SPI_NCS);
@@ -30,27 +28,58 @@ inline uint16_t endian(uint16_t val) {
 
 uint32_t mbedPrintWait = 50;
 
-void printGet(SharedSPIDevice<> radioSPI) {
+void receiveData(SharedSPIDevice<> radioSPI) {
+    while (dataReady.read() != 1) {
+        wait_ms(10);
+    }
+
     printf("Data Phase\r\n");
     wait_ms(mbedPrintWait);
-
-    radioSPI.chipSelect();
 
     int count = 0;
     uint16_t character = 0;
 
+    radioSPI.chipSelect();
     while (dataReady.read() != 0) {
         character = radioSPI.m_spi->write(0x0a0a);
         readBuffer[count] = endian(character);
         count++;
     }
-
     radioSPI.chipDeselect();
 
     printf("Received Data: ");
     for (int i = 0; i < count; i++) {
-        printf("%x ", readBuffer[i]);
+        printf("%04x ", readBuffer[i]);
     }
+    printf("\r\n");
+    wait_ms(mbedPrintWait);
+}
+
+void sendCmd (SharedSPIDevice<> radioSPI, char* command, int length) {
+    while (dataReady.read() != 1) {
+        wait_ms(10);
+    }
+
+    printf("Command Phase\r\n");
+    wait_ms(mbedPrintWait);
+
+    radioSPI.chipSelect();
+    char lastC = 0;
+    for (int i = 0; i < length; i++) {
+        char c = command[i];
+        if (lastC != 0) {
+            // NOTE:check memory space
+            uint16_t data = (c << 8) | lastC;
+            wait_ms(mbedPrintWait);
+            uint16_t returns = endian(radioSPI.m_spi->write(data)); // data write doesnt have to be flipped because of the order we merged it. Returns does.
+            printf("%x ", returns);
+            lastC = 0;
+        } else {
+            lastC = c;
+        }
+    }
+    radioSPI.chipDeselect();
+
     printf("\r\n");
     wait_ms(mbedPrintWait);
 }
@@ -61,6 +90,8 @@ int main() {
 
     {
         // clear any extraneous rx serial bytes
+
+
         Serial s(RJ_SERIAL_RXTX);
         while (s.readable()) s.getc();
 
@@ -81,86 +112,69 @@ int main() {
     wait_ms(mbedPrintWait);
 
     while (dataReady.read() != 1) {
-        // wait_ms(10);
+        wait_ms(10);
     }
 
     printf("Booted: %d\r\n", int(dataReady.read()));
     wait_ms(mbedPrintWait);
 
     // data phase (prompt)
-    printGet(radioSPI);
+    receiveData(radioSPI);
 
+
+    char* cmd0 = "$$$\r";
     // command phase
-    char* command = "AS=0,ABC\r\n";
-
-    while (dataReady.read() != 1) {
-        wait_ms(10);
-    }
-
-    // printf("Command Phase\r\n");
-    // wait_ms(mbedPrintWait);
-
-    radioSPI.chipSelect();
-    uint8_t lastC = 0;
-    for (uint8_t i = 0; i < 10; i++) {
-        char c = command[i];
-        if (lastC != 0) {
-            // NOTE:check memory space
-            uint16_t data = (c << 8) | lastC;
-            printf("Data to be sent: %x,%x\r\n", c, lastC);
-            wait_ms(mbedPrintWait);
-            uint16_t returns = endian(radioSPI.m_spi->write(data)); // data write doesnt have to be flipped because of the order we merged it. Returns does.
-            printf("%x ", returns);
-            lastC = 0;
-        } else {
-            lastC = c;
-        }
-    }
-    radioSPI.chipDeselect();
-
-    printf("\r\n");
-    wait_ms(mbedPrintWait);
-
-    // end command phase
-
-    printf("ssn: %d\r\n", int(ssn.read()));
-    wait_ms(mbedPrintWait);
-
-    printf("data ready: %d\r\n", int(dataReady.read()));
-    wait_ms(mbedPrintWait);
-
-    while (dataReady.read() != 1) {
-        // wait_ms(10);
-    }
-
-    printf("data ready: %d\r\n", int(dataReady.read()));
-    wait_ms(mbedPrintWait);
+    sendCmd(radioSPI, cmd0, 12);
 
     // data phase
-    printGet(radioSPI);
+    receiveData(radioSPI);
 
-    // uint16_t returns = radioSPI.m_spi->write(('C' << 8) + '?');
-    // printf("%x ", returns);
-    // returns = radioSPI.m_spi->write(('\n' << 8) + '\r');
-    // printf("%x ", returns);
-    // printf("\r\n");
+    char* cmd1 = "C1=Pixel\r\n";
+    // command phase
+    sendCmd(radioSPI, cmd1, 10);
 
-    // // while (dataReady.read() != 1) {
-    // //     uint8_t character = 0;
-    // //     character = radioSPI.m_spi->write('\n');
-    // //     printf("%x ", character);
-    // //     // fflush(stdout);
-    // //     wait_ms(100);
-    // // }
-    // // printf("\r\n");
+    // data phase
+    receiveData(radioSPI);
 
-    // printf("data: %d\r\n", dataReady.read());
+    char* cmd2 = "C2=Helloworld\r";
+    // command phase
+    sendCmd(radioSPI, cmd2, 16);
 
-    // // // wait_ms(10);
-    // while (dataReady.read() != 1) {
-    //     printGet(radioSPI);
-    //     wait_ms(1000);
-    // }
+    // data phase
+    receiveData(radioSPI);
+
+    char* cmd3 = "C3=3\r\n";
+    // command phase
+    sendCmd(radioSPI, cmd3, 16);
+
+    // data phase
+    receiveData(radioSPI);
+
+    char* cmd4 = "C4=1\r\n";
+    // command phase
+    sendCmd(radioSPI, cmd4, 16);
+
+    // data phase
+    receiveData(radioSPI);
+
+    char* cmd5 = "C?\r\n";
+    // command phase
+    sendCmd(radioSPI, cmd5, 4);
+
+    // data phase
+    receiveData(radioSPI);
+
+    char* cmd6 = "C0\r\n";
+    // command phase
+    sendCmd(radioSPI, cmd6, 4);
+
+    // data phase
+    receiveData(radioSPI);
+
+    while (true) {
+        wait_ms(4000);
+        // printf("Still Waiting\r\n");
+    }
 
     return 0;
 }
