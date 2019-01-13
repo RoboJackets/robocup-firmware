1848b69cbc98d0608fd6f3e61dd9d7ce1bf63f61
CollinAvidano <collin.avidano@gmail.com>
2019-01-13 15:18:45 -0500

    Revert "Merge branch 'new-radio-tcp' into new-radio"
    This reverts commit a6d4ee3c04ef8327eab44f87ba224a868fa457c8, reversing
    changes made to 7686a202d1a485ce616fa570cd9fb4c5d3426903.

--- src/hw-test/test-new-radio-ping.cpp
+++ src/hw-test/test-new-radio-ping.cpp
@@ -4,11 +4,9 @@
 #include "Rtos.hpp"
 // #include <cmsis_os.h>
 #include "SharedSPI.hpp"
-#include <chrono>
 
 #include "PinNames.h"
 #include "RobotDevices.hpp"
-#include "time.h"
 
 #define RJ_ENABLE_ROBOT_CONSOLE
 
@@ -19,74 +17,72 @@ const PinName RADIO_SPI_NCS = p20;
 const PinName RADIO_DATA_RDY = p21;
 const PinName RADIO_RST = p22;
 
-inline uint16_t endian(uint16_t val) {
-    return (val>>8) | (val<<8);
-}
-
-uint8_t readBuffer[1024];
-int packetCount = 0;
-
-Timer timer;
+uint16_t readBuffer[512];
 
 DigitalIn dataReady(RADIO_DATA_RDY);
 DigitalOut ssn(RADIO_SPI_NCS);
+
+inline uint16_t endian(uint16_t val) {
+    return (val>>8) | (val<<8);
+}
 
 uint32_t mbedPrintWait = 50;
 
 void receiveData(SharedSPIDevice<> radioSPI) {
     while (dataReady.read() != 1) {
+        wait_ms(10);
     }
 
-    // printf("Data Phase\r\n");
-    // wait_ms(mbedPrintWait);
+    printf("Data Phase\r\n");
+    wait_ms(mbedPrintWait);
 
-    packetCount = 0;
+    int count = 0;
+    uint16_t character = 0;
+
     radioSPI.chipSelect();
     while (dataReady.read() != 0) {
-        uint16_t data = radioSPI.m_spi->write(0x0a0a);
-        readBuffer[packetCount] = (uint8_t)(data);
-        readBuffer[packetCount + 1] = (uint8_t)(data >> 8);
-        packetCount += 2;
+        character = radioSPI.m_spi->write(0x0a0a);
+        readBuffer[count] = endian(character);
+        count++;
     }
     radioSPI.chipDeselect();
+
+    printf("Received Data: ");
+    for (int i = 0; i < count; i++) {
+        printf("%04x ", readBuffer[i]);
+    }
+    printf("\r\n");
+    wait_ms(mbedPrintWait);
 }
 
 void sendCmd(SharedSPIDevice<> radioSPI, char* command) {
     while (dataReady.read() != 1) {
+        wait_ms(10);
     }
 
-    // printf("Command Phase\r\n");
-    // wait_ms(mbedPrintWait);
+    printf("Command Phase\r\n");
+    wait_ms(mbedPrintWait);
 
     int length = strlen(command);
 
-    // Concatenate newline if necessary
-
-    char lastC = 0;
     radioSPI.chipSelect();
+    char lastC = 0;
     for (int i = 0; i < length; i++) {
         char c = command[i];
         if (lastC != 0) {
-            uint16_t packet = (c << 8) | lastC;
-            //Due to the fact the SPI protocol for this takes turns the return can be ignored
-            radioSPI.m_spi->write(packet);
+            // NOTE:check memory space
+            uint16_t data = (c << 8) | lastC;
+            wait_ms(mbedPrintWait);
+            uint16_t returns = endian(radioSPI.m_spi->write(data)); // data write doesnt have to be flipped because of the order we merged it. Returns does.
+            printf("%x ", returns);
             lastC = 0;
         } else {
             lastC = c;
         }
     }
     radioSPI.chipDeselect();
-}
 
-void printReadBuffer() {
-    printf("Received Data: ");
-    for (int i = 0; i < packetCount; i++) {
-        printf("%x", readBuffer[i]);
-    }
     printf("\r\n");
-
-    printf("Byte Count: %d", packetCount);
-
     wait_ms(mbedPrintWait);
 }
 
@@ -96,6 +92,8 @@ int main() {
 
     {
         // clear any extraneous rx serial bytes
+
+
         Serial s(RJ_SERIAL_RXTX);
         while (s.readable()) s.getc();
 
@@ -105,8 +103,7 @@ int main() {
 
     auto spiBus = std::make_shared<SharedSPI>(RADIO_SPI_MOSI, RADIO_SPI_MISO, RADIO_SPI_SCK);
     spiBus->format(16, 0);  // 16 bits per transfer
-    //spiBus->frequency(115200);
-    spiBus->frequency(6'000'000);
+    spiBus->frequency(115200);
 
     auto radioSPI = SharedSPIDevice<>(spiBus, RADIO_SPI_NCS, true);
     DigitalOut radioRST(RADIO_RST);
@@ -125,7 +122,6 @@ int main() {
 
     // data phase (prompt)
     receiveData(radioSPI);
-
 
     char cmdReset[] = "ZR\r\n";
     // command phase
@@ -151,7 +147,7 @@ int main() {
     receiveData(radioSPI);
 
 
-    char cmdSetSecurity[] = "C3=3\r\n";
+    char cmdSetSecurity[] = "C3=4\r\n";
     // command phase
     sendCmd(radioSPI, cmdSetSecurity);
 
@@ -165,27 +161,6 @@ int main() {
 
     // data phase
     receiveData(radioSPI);
-
-
-    printf("AboutToJoin\r\n");
-    wait_ms(mbedPrintWait);
-
-    char cmdJoinNetwork[] = "C0\r\n";
-    // command phase
-    sendCmd(radioSPI, cmdJoinNetwork);
-
-    printf("commandSent\r\n");
-    wait_ms(mbedPrintWait);
-
-    // data phase
-    receiveData(radioSPI);
-
-
-    // if ((int)readBuffer[0] == 0) {
-	// 	printf("ERROR: Failed to join network\r\n");
-    //     wait_ms(3000);
-	// 	return(0);
-    // }
 
 
     char cmdSetHumanReadable[] = "$$$\r";
@@ -202,8 +177,6 @@ int main() {
 
     // data phase
     receiveData(radioSPI);
-    printReadBuffer();
-
 
     char cmdSetMachineReadable[] = "---\r";
     // command phase
@@ -212,8 +185,43 @@ int main() {
     // data phase
     receiveData(radioSPI);
 
+    char cmdJoinNetwork[] = "C0\r\n";
+    // command phase
+    sendCmd(radioSPI, cmdJoinNetwork);
 
-    //TCP client setup
+    // data phase
+    receiveData(radioSPI);
+
+    if ((int)readBuffer[0] == 0) {
+		printf("ERROR: Failed to join network\n");
+		return(-1);
+    }
+
+    //PINGING
+    // char cmdSetPingTarget[] = "T1=192.168.1.1\r\n";
+    // // command phase
+    // sendCmd(radioSPI, cmdSetPingTarget);
+
+    // // data phase
+    // receiveData(radioSPI);
+
+
+    // char cmdShowPingSettings[] = "T?\r\n";
+    // // command phase
+    // sendCmd(radioSPI, cmdShowPingSettings);
+
+    // // data phase
+    // receiveData(radioSPI);
+
+
+    // char cmdPing[] = "T0\r\n";
+    // // command phase
+    // sendCmd(radioSPI, cmdPing);
+
+    // // data phase
+    // receiveData(radioSPI);
+
+    //TCP!!!
     char cmdSetProtocol[] = "P1=0\r\n";
     // command phase
     sendCmd(radioSPI, cmdSetProtocol);
@@ -221,48 +229,12 @@ int main() {
     // data phase
     receiveData(radioSPI);
 
-
-    char cmdSetHostIP[] = "P3=192.168.1.108\r\n";
+    char cmdJoinNetwork[] = "P3=";
     // command phase
-    sendCmd(radioSPI, cmdSetHostIP);
+    sendCmd(radioSPI, cmdJoinNetwork);
 
     // data phase
     receiveData(radioSPI);
-
-
-    char cmdSetPort[] = "P4=25565\r\n";
-    // command phase
-    sendCmd(radioSPI, cmdSetPort);
-
-    // data phase
-    receiveData(radioSPI);
-
-
-    char cmdStartClient[] = "P6=1\r\n";
-    // command phase
-    sendCmd(radioSPI, cmdStartClient);
-
-    // data phase
-    receiveData(radioSPI);
-
-
-    timer.reset();
-    for (int iter = 0; iter < 10; iter++) {
-        timer.start();
-
-        //Transport data
-        char cmdSendData[] = "S3=42\r012345678901234567890123456789012345678912";
-        // command phase
-        sendCmd(radioSPI, cmdSendData);
-
-        // data phase
-        receiveData(radioSPI);
-
-        timer.stop();
-        printf("Micro Seconds Elapsed: %d\r\n", timer.read_us());
-        wait_ms(mbedPrintWait);
-        timer.reset();
-    }
 
     printf("Done\r\n");
     return 0;
