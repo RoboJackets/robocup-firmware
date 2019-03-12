4c271a6baeb9737597a5a5e2d4a0df043ec79b53
CollinAvidano <collin.avidano@gmail.com>
2019-03-11 23:46:27 -0400

    you spin me right round baby right round like a record baby

--- lib/drivers/ism43340/ISM43340.cpp
+++ lib/drivers/ism43340/ISM43340.cpp
@@ -3,25 +3,8 @@
 
 #include <memory>
 
-#define CMD_SET_HUMAN_READABLE "$$$"
-#define CMD_SET_MACHINE_READABLE "---"
-#define CMD_RESET_SETTINGS "Z0"
 
-#define CMD_JOIN_NETWORK "C0"
-#define CMD_SET_SSID "C1="
-#define CMD_SET_PASSWORD "C2="
-#define CMD_SET_SECURITY "C3="
-#define CMD_SET_DHCP "C4="
-#define CMD_GET_CONNECTION_INFO "C?"
-
-#define CMD_SET_TRANSPORT_PROTOCOL "P1="
-#define CMD_SET_HOST_IP "P3="
-#define CMD_SET_PORT "P4="
-#define CMD_START_CLIENT "P6="
-
-#define CMD_SEND_DATA "S3="
-
-inline uint16_t swapendian(uint16_t val) {
+inline uint16_t changeEndianess(uint16_t val) {
     return (val>>8) | (val<<8);
 }
 
@@ -31,55 +14,53 @@ ISM43340::ISM43340(SpiPtrT sharedSPI, PinName nCs, PinName intPin,
     reset();
 }
 
-int ISM43340::writetospi(char* command, int commandLength) {
-    // pad command if needed
-    if (commandLength % 2 == 0) {
-        strcat(command, "\r\n");
-    } else {
-        strcat(command, "\r");
-    }
+uint32_t ISM43340::writeToSpi(uint8_t* command, uint32_t commandLength) {
 
-    // endianness conversion from the char array. It is cast to 16 bits
+    // endianess conversion from the char array. It is cast to 16 bits
     for (uint32_t i = 0; i < commandLength; i + 2) {
-        *((uint16_t*)command[i]) = endian(*((uint16_t*)command[i]));
+        *((uint16_t*)command[i]) = changeEndianess(*((uint16_t*)command[i]));
     }
 
-
-    // TODO: implement spi transfer from SPI.cpp
-
-    // Original write function
-    // uint8_t lastc = 0;
-    // radiospi.chipselect();
-    // for (int i = 0; i < command->size(); i++) {
-    //     uint8_t c = command[i];
-    //     if (lastc != 0) {
-    //         uint16_t packet = (c << 8) | lastc;
-    //         radiospi.m_spi->write(packet);
-    //         lastc = 0;
-    //     } else {
-    //         lastc = c;
-    //     }
-    // }
+    uint8_t lastc = 0;
+    radiospi.chipselect();
+    for (int i = 0; i < command->size(); i++) {
+        uint8_t c = command[i];
+        if (lastc != 0) {
+            uint16_t packet = (c << 8) | lastc;
+            radiospi.m_spi->write(packet);
+            lastc = 0;
+        } else {
+            lastc = c;
+        }
+    }
     radiospi.chipdeselect();
     return 0;
 }
 
-int ISM43340::readfromspi(uint8* readbuffer, uint32* readlength) {
-    while (dataready.read() != 1) {
-    }
+uint32_t ISM43340::readFromSpi(uint8* readbuffer, uint32* readlength) {
 
     packetcount = 0;
     radiospi.chipselect();
     while (dataready.read() != 0) {
-        uint16_t data = radiospi.m_spi->write(0x0a0a);
-        readbuffer[packetcount] = (uint8_t)(data);
-        readbuffer[packetcount + 1] = (uint8_t)(data >> 8);
+        uint16_t data = radiospi.m_spi->write(EMPTY_TRANSFER);
+        readbuffer[packetcount] = (uint8_t)(data >> 8);
+        readbuffer[packetcount+ 1] = (uint8_t)(data & 0xFF);
         packetcount += 2;
     }
     radiospi.chipdeselect();
     return 0;
 }
 
+void ISM43340::fmtCmd(BufferT& command, BufferT& payload) {
+    command.insert(payload.end(), std::make_move_iterator(payload.begin()), std::make_move_iterator(payload.end()));
+    // pad command if needed
+    if (command % 2 == 0) {
+        command[commandLength] = "\r";
+        command[commandLength] = "\n";
+    } else {
+        command[commandLength] = "\r";
+    }
+}
 
 /* COMMAND SET DEFINITIONS */
 
@@ -88,14 +69,13 @@ inline checkPrompt () {
     // read the initial prompt into a buffer
     readfromspi();
     // check the buffer for the response sequence of the prompt (Not the same as the OK)
+    readbuffer
     // you could probably have read spi return an int as a status code for both cant decide
 }
 
 // This command will probably not be used but is here as an example command
 // Set Human Readable
-inline setHumanReadable() {
-    std::string compositeString(CMD_SET_HUMAN_READABLE);
-
+inline setHumanReadable () {
     // write composite string to the radio
     writetospi(&compositeString);
     // read to global buffer
@@ -116,57 +96,53 @@ inline setHumanReadable() {
 
 //Show connection status
 
+//Send Data TCP
+inline sendDataSPI (BufferT txBuffer) {
+
+    // write composite string to the radio
+    writetospi(&compositeString);
+    // read to global buffer
+    readfromspi();
+}
+
 // This one is here as an example as well
 //Show network settings
 inline showNetworkSettings() {
     std::string compositeString(CMD_SHOW_NETWORK_SETTINGS);
 
-    // formats command adding /r and /n when needed
-    formatCommand(&compositeString);
-
-    // write composite string to the radio
     writetospi(&compositeString);
-    // read to global buffer. This needs to be returned to the user
     readfromspi();
     // the returned buffer from this is of unknown length. However it is well bounded by the length of possible network information
 }
 
 
 int32_t ISM43340::sendPacket(const rtp::Packet* pkt) {
-    // Return failutre if not initialized
+    // Return failure if not initialized
     if (!m_isInit) return COMM_FAILURE;
 
-
-    // dont know how much of this needs to be kept. Soccer may actually want the split between body and head of the sent packet. I dont know
     BufferT txBuffer;
 
-    const auto bufferSize = 9 + pkt->size() + 2;
+    const auto bufferSize = 3 + pkt->size() + 2;
     txBuffer.reserve(bufferSize);
-
-    // MAC layer header for Decawave
-    txBuffer.insert(txBuffer.end(),
-                    {0x41, 0x88, 0x00, 0xCA, 0xDE, pkt->header.address, 0x00,
-                     static_cast<uint8_t>(m_address), 0x00});
-
-    ASSERT(txBuffer.size() == 9);
 
     const auto headerFirstPtr = reinterpret_cast<const uint8_t*>(&pkt->header);
     const auto headerLastPtr = headerFirstPtr + rtp::HeaderSize;
+
+    const auto cmdFirstPtr = reinterpret_cast<const uint8_t*>(&pkt->header);
+    const auto cmdLastPtr = headerFirstPtr + rtp::HeaderSize;
 
     // insert the rtp header
     txBuffer.insert(txBuffer.end(), headerFirstPtr, headerLastPtr);
     // insert the rtp payload
     txBuffer.insert(txBuffer.end(), pkt->payload.begin(), pkt->payload.end());
-    // insert padding for CRC
-    txBuffer.insert(txBuffer.end(), {0x00, 0x00});
 
-    /* This is the Decawave's send buffer code
-    dwt_writetxdata(txBuffer.size(), txBuffer.data(), 0);
-    dwt_writetxfctrl(txBuffer.size(), 0, 0);
-    */
-    // For us we simply need the original buffer and the original buffer length
-    // We then have to convert it to a string and add the command start (S3=N\rDATA) where N the original buffer length and DATA is the original buffer as a string
-    // The SPI write will need the final length of the command Including the length to represent n and be properly padded
+    // ISM Stuff
+    txBuffer.insert(CMD_SEND_DATA, 0);
+
+    writeToSpi(txBuffer.data(), txBuffer.size());
+
+
+
 
     // This error code can be replaced with the return of calling send transport data
     const auto sentStatus =
---
+++ lib/drivers/ism43340/ISM43340.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "CommLink.hpp"
+
+namespace ISMConstants {
+    static const std::string OK = "OK\r\n\>";
+    static const std::string PROMPT = "\r\n>";
+
+    static const std::string EVEN_DELIMITER = "\r\n";
+    static const std::string ODD_DELIMITER = "\r";
+
+    static const std::string EMPTY_TRANSFER = "\n\n";
+
+    static const std::string CMD_SET_HUMAN_READABLE = "$$$";
+    static const std::string CMD_SET_MACHINE_READABLE = "---";
+    static const std::string CMD_RESET_SETTINGS = "Z0";
+
+    static const std::string CMD_JOIN_NETWORK = "C0";
+    static const std::string CMD_SET_SSID = "C1=";
+    static const std::string CMD_SET_PASSWORD = "C2=";
+    static const std::string CMD_SET_SECURITY = "C3=";
+    static const std::string CMD_SET_DHCP = "C4=";
+    static const std::string CMD_GET_CONNECTION_INFO = "C?";
+
+    static const std::string CMD_SET_TRANSPORT_PROTOCOL = "P1=";
+    static const std::string CMD_SET_HOST_IP = "P3=";
+    static const std::string CMD_SET_PORT = "P4=";
+    static const std::string CMD_START_CLIENT = "P6=";
+
+    static const std::string CMD_SEND_DATA = "S3=";
+}
+
+class ISM43340 : public CommLink {
+public:
+    ISM43340(SpiPtrT sharedSPI, PinName nCs, PinName intPin, PinName _nReset);
+
+    virtual int32_t sendPacket(const rtp::Packet* pkt) override;
+
+    virtual BufferT getData() override;
+
+    // virtual void reset() override { dwt_softreset(); }
+    virtual void reset() override;
+
+    virtual int32_t selfTest() override;
+
+    virtual bool isConnected() const override { return m_isInit; }
+
+    virtual void setAddress(int addr) override;
+
+    virtual int writeToSpi(char* command, int commandLength);
+
+    virtual int readFromSpi();
+
+    void setLED(bool ledOn) { dwt_setleds(ledOn); };
+
+private:
+    InterruptIn dataReady();
+
+    BufferPtrT m_rxBufferPtr = nullptr;
+    BufferPtrT m_txBufferPtr = nullptr;
+    uint32_t m_chipVersion;
+    bool m_isInit = false;
+    DigitalOut nReset;
+
+    void getDataSuccess(const dwt_cb_data_t* cb_data);
+    void getDataFail(const dwt_cb_data_t* cb_data);
+};
