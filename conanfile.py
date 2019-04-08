9d140953c53c6248e8926e129b39b9ec4013f182
Evan Peterson <77evan@gmail.com>
2019-04-08 17:59:08 -0400

    conan create worksish

--- conanfile.py
+++ conanfile.py
@@ -6,11 +6,19 @@ class MtrainConan(ConanFile):
     url = "https://github.com/RoboJackets/mtrain-firmware"
     description = "mTrain description"
     # settings = "os", "compiler", "build_type", "arch"
-    settings = {"os": None, "build_type": None, "compiler": None, "arch": ["armv7e-m/hf"]}
+    settings = {"build_type": None, "compiler": None, "arch": ["armv7hf"]}
     generators = "cmake"
+    exports_sources = ['CMakeLists.txt', 'API*', 'BSP*', 'external*']
 
     def build(self):
         cmake = CMake(self)
-        cmake.verbose = True
+        # cmake.verbose = True
         cmake.configure()
         cmake.build()
+
+    def imports(self):
+        self.copy(pattern='*.so*', dst='lib', src='lib')
+
+    def package(self):
+        self.copy(pattern='*.h', dst='API/c/Inc', src='API/c/Inc')
+        self.copy(pattern='*.h', dst='API/cpp/Inc', src='API/cpp/Inc')
--- tests/c/usb_serial.c
+++ tests/c/usb_serial.c
@@ -1,40 +1,42 @@
+
+
 #include "mtrain.h"
 #include  <unistd.h>
 
-USBD_HandleTypeDef USBD_Device;
+// USBD_HandleTypeDef USBD_Device;
 
 int main(void)
 {
-    USBD_Init(&USBD_Device, &VCP_Desc, 0);
-    USBD_RegisterClass(&USBD_Device, USBD_CDC_CLASS);
-    USBD_CDC_RegisterInterface(&USBD_Device, &USBD_CDC_fops);
-    USBD_Start(&USBD_Device);
+    // USBD_Init(&USBD_Device, &VCP_Desc, 0);
+    // USBD_RegisterClass(&USBD_Device, USBD_CDC_CLASS);
+    // USBD_CDC_RegisterInterface(&USBD_Device, &USBD_CDC_fops);
+    // USBD_Start(&USBD_Device);
 
-    GPIO_InitTypeDef GPIO_InitStruct;
+    // GPIO_InitTypeDef GPIO_InitStruct;
 
-    GPIO_InitStruct.Pin = LED1_PIN;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
-    HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_SET);
+    // GPIO_InitStruct.Pin = LED1_PIN;
+    // GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    // GPIO_InitStruct.Pull = GPIO_PULLUP;
+    // GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    // HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+    // HAL_GPIO_WritePin(LED_PORT, LED1_PIN, GPIO_PIN_SET);
 
-    fflush(stdout);
+    // fflush(stdout);
 
-    int count = 0;
-    while(1) {
-        HAL_Delay(100);
-        printf("\r%d", count);
-        fflush(stdout);
-        count++;
-    }
+    // int count = 0;
+    // while(1) {
+    //     HAL_Delay(100);
+    //     printf("\r%d", count);
+    //     fflush(stdout);
+    //     count++;
+    // }
 }
 
-int _write(int file, char *data, int len)
-{
-    if (file == STDOUT_FILENO) {
-        USBD_CDC_SetTxBuffer(&USBD_Device, (uint8_t*)data, len);
-        USBD_CDC_TransmitPacket(&USBD_Device);
-    }
-    return 0;
-}
+// int _write(int file, char *data, int len)
+// {
+//     if (file == STDOUT_FILENO) {
+//         USBD_CDC_SetTxBuffer(&USBD_Device, (uint8_t*)data, len);
+//         USBD_CDC_TransmitPacket(&USBD_Device);
+//     }
+//     return 0;
+// }
