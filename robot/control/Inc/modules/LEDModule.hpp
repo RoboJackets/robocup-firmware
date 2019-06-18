d0fd8e6732351d3c24ecf079bd15709f553480b4
Joseph Neiger <fcdneiger@gmail.com>
2019-06-18 18:38:07 -0400

    Added the dot start led

--- robot/control/Inc/modules/LEDModule.hpp
+++ robot/control/Inc/modules/LEDModule.hpp
@@ -5,6 +5,7 @@
 
 #include "DigitalOut.hpp"
 #include "I2C.hpp"
+#include "SPI.hpp"
 #include "GenericModule.hpp"
 #include "MicroPackets.hpp"
 #include "drivers/MCP23017.hpp"
@@ -12,7 +13,7 @@
 class LEDModule : public GenericModule {
 public:
     // How many times per second this module should run
-    static constexpr float freq = 1.0f; // Hz
+    static constexpr float freq = 100.0f; // Hz
     static constexpr uint32_t period = static_cast<uint32_t>(1000 / freq);
 
     // How long a single call to this module takes
@@ -21,6 +22,7 @@ public:
     LEDModule(std::shared_ptr<MCP23017> ioExpander,
               BatteryVoltage *const batteryVoltage,
               FPGAStatus *const fpgaStatus,
+              KickerInfo *const kickerInfo,
               RadioError *const radioError);
 
     virtual void entry(void);
@@ -54,11 +56,29 @@ public:
     void missedModuleRun();
 
 private:
+    /**
+     * Sets the color of the two dot stars
+     *
+     * @param led0 0..7 red
+     *             8..15 green
+     *             16..23 blue
+     *             24..31 don't care
+     * 
+     * @param led1 0..7 red
+     *             8..15 green
+     *             16..23 blue
+     *             24..31 don't care
+     */
+    void setColor(uint32_t led0, uint32_t led1);
+
     const static uint16_t IOExpanderErrorLEDMask = 0xFF00;
 
     BatteryVoltage *const batteryVoltage;
     FPGAStatus *const fpgaStatus;
+    KickerInfo *const kickerInfo;
     RadioError *const radioError;
+
+    SPI dot_star_spi;
 
     std::shared_ptr<MCP23017> ioExpander;
 
--- robot/control/Src/modules/LEDModule.cpp
+++ robot/control/Src/modules/LEDModule.cpp
@@ -4,11 +4,15 @@
 LEDModule::LEDModule(std::shared_ptr<MCP23017> ioExpander,
                      BatteryVoltage *const batteryVoltage,
                      FPGAStatus *const fpgaStatus,
+                     KickerInfo *const kickerInfo,
                      RadioError *const radioError)
     : batteryVoltage(batteryVoltage), fpgaStatus(fpgaStatus),
-      radioError(radioError), ioExpander(ioExpander),
+      kickerInfo(kickerInfo), radioError(radioError),
+      dot_star_spi(DOT_STAR_SPI_BUS, std::nullopt, 1'000'000), ioExpander(ioExpander),
       leds({LED1, LED2, LED3, LED4}),
       missedSuperLoopToggle(false), missedModuleRunToggle(false) {
+
+    setColor(0xFFFFFF, 0xFFFFFF);
 
     ioExpander->writeMask(static_cast<uint16_t>(~IOExpanderErrorLEDMask), IOExpanderErrorLEDMask);
 }
@@ -22,6 +26,8 @@ void LEDModule::entry(void) {
         for (int i = 0; i < 5; i++) {
             errors |= (1 << motors[i]);
         }
+
+        setColor(0x0000FF, 0xFFFFFF);
     } else { 
         for (int i = 0; i < 5; i++) {
             errors |= (fpgaStatus->motorHasErrors[i] << motors[i]);
@@ -32,27 +38,35 @@ void LEDModule::entry(void) {
         errors |= (1 << ERR_LED_RADIO);
     }
 
-    ioExpander->writeMask(~errors, IOExpanderErrorLEDMask);
-    printf("%4x \r\n", errors);
+    if (kickerInfo->isValid || kickerInfo->kickerHasError) {
+        errors |= (1 << ERR_LED_KICK);
+    }
+
+    ioExpander->writeMask(errors, IOExpanderErrorLEDMask);
+    leds[0].toggle();
 }
 
 void LEDModule::fpgaInitialized() {
     // neo pixel stuff
+    setColor(0xFFFF00, 0xFFFFFF);
     leds[0] = 1;
 }
 
 void LEDModule::radioInitialized() {
     // neo pixel stuff
+    setColor(0xFF00FF, 0xFFFFFF);
     leds[1] = 1;
 }
 
 void LEDModule::kickerInitialized() {
     // neo pixel stuff
+    setColor(0x00FFFF, 0xFFFFFF);
     leds[2] = 1;
 }
 
 void LEDModule::fullyInitialized() {
     // green neo pixel
+    setColor(0x00FF00, 0x00FF00);
     leds[3] = 1;
 }
 
@@ -62,16 +76,51 @@ void LEDModule::missedSuperLoop() {
     }
 
     missedSuperLoopToggle = !missedSuperLoopToggle;
+
+    // Orange
+    setColor(0x3376DC, 0xFFFFFF);
 }
 
 void LEDModule::missedModuleRun() {
     for (int i = 0; i < 4; i++) {
         if (i % 2 == 0) {
-            leds[i] = missedModuleRunToggle;
+            leds[i] = (int)missedModuleRunToggle;
         } else {
-            leds[i] = !missedSuperLoopToggle;
+            leds[i] = (int)!missedSuperLoopToggle;
         }
     }
 
     missedModuleRunToggle = !missedModuleRunToggle;
+
+    // Yellow
+    setColor(0x3FD0F4, 0xFFFFFF);
+}
+
+void LEDModule::setColor(uint32_t led0, uint32_t led1) {
+    // 0 - 31
+    uint8_t brightness = 2 | 0xE0;
+
+    std::vector<uint8_t> data;
+
+    data.push_back(0x00);
+    data.push_back(0x00);
+    data.push_back(0x00);
+    data.push_back(0x00);
+
+    data.push_back(brightness);
+    data.push_back((led0 >> 16) & 0xFF);
+    data.push_back((led0 >> 8) & 0xFF);
+    data.push_back((led0 >> 0) & 0xFF);
+    
+    data.push_back(brightness);
+    data.push_back((led1 >> 16) & 0xFF);
+    data.push_back((led1 >> 8) & 0xFF);
+    data.push_back((led1 >> 0) & 0xFF);
+    
+    data.push_back(0xFF);
+    data.push_back(0xFF);
+    data.push_back(0xFF);
+    data.push_back(0xFF);
+
+    dot_star_spi.transmit(data);
 }
