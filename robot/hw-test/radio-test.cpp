0c24a98cb4525d7e5a86838d19821712fc6ff240
Joseph Neiger <fcdneiger@gmail.com>
2019-08-02 15:13:01 -0400

    Removed usb

--- robot/hw-test/radio-test.cpp
+++ robot/hw-test/radio-test.cpp
@@ -6,19 +6,7 @@
 #include "drivers/ISM43340.hpp"
 #include "bsp.h"
 
-//for debugging
-#include  <unistd.h>
-USBD_HandleTypeDef USBD_Device;
-
-
 int main() {
-    USBD_Init(&USBD_Device, &VCP_Desc, 0);
-    USBD_RegisterClass(&USBD_Device, USBD_CDC_CLASS);
-    USBD_CDC_RegisterInterface(&USBD_Device, &USBD_CDC_fops);
-    USBD_Start(&USBD_Device);
-
-    HAL_Delay(5000);
-
     DigitalOut l1 = DigitalOut(LED1);
 
     l1 = 1;
@@ -40,17 +28,4 @@ int main() {
       l1.toggle();
       HAL_Delay(100);
   }
-}
-
-extern "C" {
-
-int _write(int file, char *data, int len)
-{
-    if (file == STDOUT_FILENO && USBD_Device.dev_state == USBD_STATE_CONFIGURED ) {
-        USBD_CDC_SetTxBuffer(&USBD_Device, (uint8_t*)data, len);
-        USBD_CDC_TransmitPacket(&USBD_Device);
-    }
-    return 0;
-}
-
 }
