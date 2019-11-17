db4df22c62a7d9225512c87356a8a109685cf78c
Evan Peterson <77evan@gmail.com>
2019-11-17 15:14:26 -0500

    Fix package to include rtos (#42)
    * fix package to include rtos
    
    * bump to 1.0.0

--- conanfile.py
+++ conanfile.py
@@ -2,7 +2,7 @@ from conans import ConanFile, CMake
 
 class MtrainConan(ConanFile):
     name = "mTrain"
-    version = "0.1.6"
+    version = "1.0.0"
     url = "https://github.com/RoboJackets/mtrain-firmware"
     description = "mTrain description"
     license = "Apache"
@@ -28,7 +28,9 @@ class MtrainConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.includedirs = ['API/c/Inc', 'API/cpp/Inc', 'BSP/config/hal', 'BSP/config/usb', 'BSP/Inc',
-            'external/CMSIS/Device/STM32F7xx/Include', 'external/CMSIS/Include', 'external/middleware', 'external/STM32F7xx_HAL_Drivers/Inc']
+            'external/CMSIS/Device/STM32F7xx/Include', 'external/CMSIS/Include', 'external/middleware', 'external/STM32F7xx_HAL_Drivers/Inc',
+            'external/middleware/FreeRTOS/src/include', 'external/middleware/FreeRTOS/src/CMSIS_RTOS', 'external/middleware/FreeRTOS/src/portable/GCC/ARM_CM7/r0p1',
+            'external/middleware/STM32_USB_Device_Library/Core/Inc', 'external/middleware/STM32_USB_Device_Library/Class/CDC/Inc']
         self.cpp_info.exelinkflags.append('-T %s/flash.ld' % self.package_folder)
         self.cpp_info.libs = ['STM32F7xx', 'STM32F7_API_CPP', 'STM32F7_API_C', 'STM32_USB_Device', 'STM32F7xx_HAL', 'DSP', 'FreeRTOS']
 
