90aba929ec66c75f9c420f9fd2513d03a31b165c
Kyle Stachowicz <kylestach99@gmail.com>
2020-01-25 14:53:52 -0500

    Increase heap size and use heap_3 (malloc()) for RTOS

--- external/middleware/FreeRTOS/src/include/FreeRTOSConfig.h
+++ external/middleware/FreeRTOS/src/include/FreeRTOSConfig.h
@@ -130,6 +130,7 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil        1
 #define INCLUDE_vTaskDelay             1
 #define INCLUDE_xTaskGetSchedulerState 1
+#define INCLUDE_xSemaphoreGetMutexHolder 1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
