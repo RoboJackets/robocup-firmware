14b05cc2d5ccda6103486fe1d038149f095a86fb
tcontis <thomas@contis.org>
2020-06-03 14:54:46 -0400

    Added documentation for BatteryModule

--- robot/control/Inc/modules/BatteryModule.hpp
+++ robot/control/Inc/modules/BatteryModule.hpp
@@ -9,42 +9,46 @@
 class BatteryModule : public GenericModule {
 public:
     /**
-     * Number of times per second (frequency) that BatteryModule should run, in Hz.
+     * Number of times per second (frequency) that BatteryModule should run (Hz).
      */
     static constexpr float kFrequency = 1.0f;
 
     /**
-     * Number of seconds elapsed (period) between BatteryModule runs, in milliseconds
+     * Number of seconds elapsed (period) between BatteryModule runs (milliseconds)
      */
     static constexpr std::chrono::milliseconds kPeriod{static_cast<int>(1000 / kFrequency)};
 
     /**
      * Priority used by RTOS
+     *
      * Uses the default priority of 1.
      */
     static constexpr int kPriority = 1;
 
     /**
      * Constructor for BatteryModule
-     * @param Object storing information from `battery`
+     *
+     * @param batteryVoltage Object storing information from `battery`
      */
     explicit BatteryModule(LockedStruct<BatteryVoltage>& batteryVoltage);
 
     /**
-    * Code to run when called by RTOS
-    * Updates `batteryVoltage` with new information from `battery`
-    */
+     * Code to run when called by RTOS
+     *
+     * Updates `batteryVoltage` with new information from `battery`
+     */
     void entry() override;
 
 private:
     /**
      * Stores information from `battery`
+     *
      * Set by constructor
      */
     LockedStruct<BatteryVoltage>& batteryVoltage;
 
     /**
-    * Interfaces with physical battery to get updates for `batteryVoltage`
-    */
+     * Interfaces with physical battery to get updates for `batteryVoltage`
+     */
     Battery battery;
 };
