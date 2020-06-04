3e0a73e5d3e93b81c54c828ad568cb6d4a350db3
tcontis <thomas@contis.org>
2020-06-04 14:20:01 -0400

    Added documentation for FPGAModule

--- robot/control/Inc/modules/FPGAModule.hpp
+++ robot/control/Inc/modules/FPGAModule.hpp
@@ -12,18 +12,43 @@
 
 class FPGAModule : public GenericModule {
 public:
-    // How many times per second this module should run
-    static constexpr float kFrequency = 100.0f; // Hz
+    /**
+     * Number of times per second (frequency) that FPGAModule should run (Hz)
+     */
+    static constexpr float kFrequency = 100.0f;
+
+    /**
+     * Number of seconds elapsed (period) between FPGAModule runs (milliseconds)
+     */
     static constexpr std::chrono::milliseconds kPeriod{static_cast<int>(1000 / kFrequency)};
+
+    /**
+     * Priority used by RTOS
+     */
     static constexpr int kPriority = 2;
 
+    /**
+    * Constructor for FPGAModule
+    * @param spi Pointer to SPI object which handles communication on SPI bus
+    * @param motorCommand Packet of data containing wheel motor duty cycles and dribbler rotation speed
+    * @param fpgaStatus Packet of data containing whether motors or FPGA have errors
+    * @param motorFeedback Packet of data containing encoder counts and currents to each wheel motor
+    */
     FPGAModule(std::unique_ptr<SPI> spi,
                LockedStruct<MotorCommand>& motorCommand,
                LockedStruct<FPGAStatus>& fpgaStatus,
                LockedStruct<MotorFeedback>& motorFeedback);
 
+    /**
+     * Code which initializes module
+     */
     void start() override;
 
+    /**
+     * Code to run when called by RTOS
+     *
+     * Updates motor voltages and feedback (currents/encoder counts)
+     */
     void entry() override;
 
 private:
