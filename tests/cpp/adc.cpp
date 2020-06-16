c921ec2f10e0b7f61d51d79b4ad9f97f490b91c1
CollinAvidano <collin.avidano@gmail.com>
2020-06-16 16:54:56 -0400

    updated test to use digitial io pin

--- tests/cpp/adc.cpp
+++ tests/cpp/adc.cpp
@@ -4,12 +4,15 @@ using namespace std;
 
 int main(void) {
   DigitalOut leds[] = {(LED1), (LED2), (LED3), (LED4)};
+  DigitalOut output {p34};
   AnalogIn pin(adc3); //need analog pins
+
+  output.write(true);
 
   while(true) {
     uint32_t adc_val = pin.read();
+    printf("Raw Value %u\r\n", adc_val);
     HAL_Delay(100);
-    printf("%u\r\n", adc_val);
     leds[0].toggle();
     if (adc_val > 0) {
       leds[1].toggle();
