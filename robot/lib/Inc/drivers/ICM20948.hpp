06ce70465a0205fad6b1f0608b5ecb46aac12c6c
Tohwx <weixiongtoh@gmail.com>
2020-08-25 12:18:26 +0800

    added IMU drivers on main branch

---
+++ robot/lib/Inc/drivers/ICM20948.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "mtrain.hpp"
+#include "SPI.hpp"
+#include "LockedStruct.hpp"
+#include <memory>
+
+class ICM20948 {
+public:
+    ICM20948(LockedStruct<SPI>& imuSPI, PinName cs_pin);
+
+    bool initialize();
+
+    double gyro_z();
+    
+    double accel_x();
+
+    double accel_y();
+
+private:
+    void write_register(uint8_t bank, uint8_t address, uint8_t value);
+
+    uint8_t read_register(uint8_t bank, uint8_t address);
+
+    void chip_select(bool cs_state);
+
+    LockedStruct<SPI>& imuSPI;
+
+    DigitalOut nCs;
+};
+
