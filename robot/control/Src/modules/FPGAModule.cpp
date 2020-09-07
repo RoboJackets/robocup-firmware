9ebbb43a348cb3a90b2fd59a6adf8f0892bf9e5c
Kyle Stachowicz <kylestach99@gmail.com>
2020-09-07 13:27:01 -0400

    More cleanup

--- robot/control/Src/modules/FPGAModule.cpp
+++ robot/control/Src/modules/FPGAModule.cpp
@@ -116,16 +116,6 @@ void FPGAModule::entry() {
     {
         auto motorFeedbackLock = motorFeedback.lock();
 
-        // Force dt to be nonzero so nan's don't filter up. If it's too small
-        // or too large just exit early and reset velocities.
-        if (dt < 0.0001 || dt > 0.015) {
-            for (int i = 0; i < 4; i++) {
-                motorFeedbackLock->encoders[i] = 0.0f;
-            }
-            motorFeedbackLock->isValid = false;
-            return;
-        }
-
         // Convert encoders to rad/sec from enc ticks since last reading
         for (int i = 0; i < 4; i++) {
             // (rad / s) = (enc) * (rev / enc) * (rad / rev) * (1 / sec)
