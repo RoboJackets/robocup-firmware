f71b2ac490378c8a3ee415ef4399cea8353849be
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2020-10-14 23:36:59 -0400

    Fixed bad IMU:

--- robot/control/Src/motion-control/RobotEstimator.cpp
+++ robot/control/Src/motion-control/RobotEstimator.cpp
@@ -21,7 +21,7 @@ RobotEstimator::RobotEstimator(uint32_t dt_us) {
     //H = [bot2Wheel;
     //      0, 0, 1]
     H.block<4, 3>(0, 0) = RobotModel::get().BotToWheel.cast<float>();
-    H.block<1, 3>(4, 0) << 0, 0, 0;
+    H.block<1, 3>(4, 0) << 0, 0, 1;
 
     Q.setIdentity();
     Q *= 3.0*processNoise / (dt*dt);
@@ -51,14 +51,21 @@ void RobotEstimator::update(Eigen::Matrix<float, numOutputs, 1> z) {
     // K = P*H'*S^-1
     // x_hat += K*y
     // P = (I - K*H)*P
-    Eigen::Matrix<float, numOutputs, 1> y = z - H*x_hat;
+    Eigen::Matrix<float, numOutputs, 1> zhat = H*x_hat;
+    Eigen::Matrix<float, numOutputs, 1> y = z - zhat;
+    y(0) = y(1) = y(2) = y(3) = 0;
     Eigen::Matrix<float, numStates,  numOutputs> K;
     z(4) = x_hat(2);
     K << 1.83624980e-01, -2.29806179e-01, -2.29806179e-01, 1.83624980e-01, -2.13457449e-04,
          3.07632598e-01, 2.76060529e-01, -2.76060529e-01, -3.07632598e-01, -2.99584147e-18,
          -6.86033585e-02, -6.02908809e-02, -6.02908809e-02, -6.86033585e-02, 2.38627208e-02;
 
-    x_hat += K * 0.01 * y;
+    x_hat += K * y;
+    static int i = 0;
+    if (i++ % 20 == 0) {
+        printf("xhat: %f %f %f\r\n", x_hat(0), x_hat(1), x_hat(2));
+        printf("zhat: %f %f %f\r\n", zhat(0), zhat(1), zhat(2), zhat(3), zhat(4));
+    }
 }
 
 void RobotEstimator::getState(Eigen::Matrix<float, numStates, 1>& state) {
