55f5b1c3e87ed8c1cd9c60f60cb7ef662bd00391
Arvind Srinivasan <arvindsrinivasan@gatech.edu>
2020-10-17 19:03:28 -0400

    Title

--- robot/control/Src/motion-control/RobotEstimator.cpp
+++ robot/control/Src/motion-control/RobotEstimator.cpp
@@ -53,18 +53,19 @@ void RobotEstimator::update(Eigen::Matrix<float, numOutputs, 1> z) {
     // P = (I - K*H)*P
     Eigen::Matrix<float, numOutputs, 1> zhat = H*x_hat;
     Eigen::Matrix<float, numOutputs, 1> y = z - zhat;
-    y(0) = y(1) = y(2) = y(3) = 0;
     Eigen::Matrix<float, numStates,  numOutputs> K;
-    z(4) = x_hat(2);
     K << 1.83624980e-01, -2.29806179e-01, -2.29806179e-01, 1.83624980e-01, -2.13457449e-04,
          3.07632598e-01, 2.76060529e-01, -2.76060529e-01, -3.07632598e-01, -2.99584147e-18,
-         -6.86033585e-02, -6.02908809e-02, -6.02908809e-02, -6.86033585e-02, 2.38627208e-02;
+         -6.86033585e-01, -6.02908809e-01, -6.02908809e-01, -6.86033585e-01, 2.38627208e-01;
 
     x_hat += K * y;
     static int i = 0;
     if (i++ % 20 == 0) {
         printf("xhat: %f %f %f\r\n", x_hat(0), x_hat(1), x_hat(2));
-        printf("zhat: %f %f %f\r\n", zhat(0), zhat(1), zhat(2), zhat(3), zhat(4));
+        printf("zhat: %f %f %f %f %f\r\n", zhat(0), zhat(1), zhat(2), zhat(3), zhat(4));
+        printf("z: %f %f %f %f %f\r\n", z(0), z(1), z(2), z(3), z(4));
+        printf("y: %f %f %f %f %f\r\n", y(0), y(1), y(2), y(3), y(4));
+        printf("\r\n");
     }
 }
 
