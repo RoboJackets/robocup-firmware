4cf2f0607a0f7c4d95e9013e89faa544e66a3412
mdas64 <missmilid@gmail.com>
2021-10-24 21:37:29 -0400

    Merge branch 'robot-shell-id' of https://github.com/RoboJackets/robocup-firmware into robot-shell-id

--- control/src/drivers/TSL2572.cpp
+++ control/src/drivers/TSL2572.cpp
@@ -5,9 +5,13 @@ TSL2572::TSL2572(LockedStruct<I2C>& sharedI2C, int i2cAddress)
     : _i2c(sharedI2C), _i2cAddress(i2cAddress) {
 }
 
+// write to a specific register
 void TSL2572::writeRegister(TSL2572::Register regAddress, uint16_t data) {
     auto i2c_lock = _i2c.lock();
     std::vector<uint8_t> buffer{static_cast<uint8_t>(data & 0xff),
                                 static_cast<uint8_t>(data >> 8)};
     i2c_lock->transmit(_i2cAddress, regAddress, buffer);
 }
+
+// TODO: write the function that reads a specific register
+
