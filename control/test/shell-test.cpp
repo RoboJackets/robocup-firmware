36bb62b28264f882336410c81b6ded8bf1ba89f4
Ritarka <ritarkasamanta@gmail.com>
2022-02-08 20:15:21 -0500

    A message

--- control/test/shell-test.cpp
+++ control/test/shell-test.cpp
@@ -3,5 +3,24 @@
 
 int main()
 {
-    // initialize 4 sensor objects
+    TSL2572 lightSensor = new TSL2572(57, 0);
+    //PCA9858PWJ pca = new PCA9858PWJ(242, 0);
+
+    auto i2c_lock = _i2c.lock();
+    uint32_t values[4];
+    uint8_t channel = 2 for (int i = 0; i < 4; i++)
+    {
+        //channel selector
+        bool error = writeByte(226, 0, channel);
+
+        //reading from light sensor
+        values[i] = lightSensor.caculateLux(0);
+
+        channel << 1;
+    }
+    while (true) {
+        for (uint32_t value : values) {
+            printf("Here's a fun value =D %d", value);
+        }
+    }
 }
