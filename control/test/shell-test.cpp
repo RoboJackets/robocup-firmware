ef8a599c8e96a236b02b776f25d6e0c85ba36e95
schalla33 <schalla33@gatech.edu>
2022-03-24 12:40:52 -0400

    updated test

--- control/test/shell-test.cpp
+++ control/test/shell-test.cpp
@@ -21,71 +21,71 @@
 #include "delay.h"
 #include "LockedStruct.hpp"
 
-
 #define CONTROL 0xE2
 
 int main()
 {
-    
-    
-    I2C sharedI2C(I2CBus::I2CBus1);
-    
-    
-    //std::shared_ptr<I2C> I2C = std::make_shared<I2C>(I2CBus, std::nullopt, 1'000'000);
 
+  I2C sharedI2C(I2CBus::I2CBus1);
 
-    
+  // std::shared_ptr<I2C> I2C = std::make_shared<I2C>(I2CBus, std::nullopt, 1'000'000);
 
-   
-    PCA9858PWJ mux(sharedI2C); 
+  PCA9858PWJ mux(sharedI2C);
 
-    
-    
-    TSL2572 lightSensor1(0, sharedI2C);
-    TSL2572 lightSensor2(2, sharedI2C);
-    TSL2572 lightSensor3(3, sharedI2C); 
-    TSL2572 lightSensor4(4, sharedI2C);
- 
-    
+  TSL2572 lightSensor1(0, sharedI2C);
+  TSL2572 lightSensor2(2, sharedI2C);
+  TSL2572 lightSensor3(3, sharedI2C);
+  TSL2572 lightSensor4(4, sharedI2C);
 
-    int i = 4;
+  int i = 4;
+  mux.writeRegister(0b00010000);
+  while (true)
+  {
+    DWT_Delay(100);
+    // printf("ID: %x\n", lightSensor1.readRegister(TSL2572::ID));
+    DWT_Delay(100);
+    printf("CO: %x\n", lightSensor1.readRegister(TSL2572::ENABLE));
+    DWT_Delay(100);
+    printf("C0H: %x\n", lightSensor1.readRegister(TSL2572::C0DATAH));
+    DWT_Delay(100);
+    printf("C1: %x\n", lightSensor1.readRegister(TSL2572::C1DATA));
+    DWT_Delay(100);
+    printf("C1H: %x\n", lightSensor1.readRegister(TSL2572::C1DATAH));
+    DWT_Delay(100);
+    // for (i = 4; i < 8; i++)
 
-    while (true) {
-       
-      for (i = 4; i < 8; i++) {
-        mux.writeRegister(0b00010000 << (i - 4)); 
-        printf("%d:\t%d\n", lightSensor1.readRegister(TSL2572::C0DATAH), i - 4); 
-     } 
-    }
-   
-    // printf("%d:\t%d\n", lightSensor2.readRegister(TSL2572::C0DATA), 2); 
-    // printf("%d:\t%d\n", lightSensor3.readRegister(TSL2572::C0DATA), 3); 
-    // printf("%d:\t%d\n", lightSensor4.readRegister(TSL2572::C0DATA), 4); 
-    
-
-    
-    // for (i = 4; i < 8; i++) {
-    //     //mux.writeRegister(0b00010000 << (i - 4)); 
-    //     printf("%d:\t%d\n", lightSensor.readRegister(TSL2572::C0DATA), i - 4); 
-    // }
-    // auto i2c_lock = _i2c.lock();
-    // uint32_t values[4];
-    // uint8_t channel = 2;
-    // for (int i = 0; i < 4; i++)
     // {
-    //     //channel selector
-    //     bool error = writeByte(226, 0, channel);
-
-    // // while(true) {
-    // //     printf("lux: %ld\n", lightSensor.calculateLux(1));
-    // // }
-    // return 0; 
-
-    //     channel << 1;
+    //   mux.writeRegister(0b00010000 << (i - 4));
+    //   printf("%d:\t%x\n", i - 4, lightSensor1.readRegister(TSL2572::ID));
     // }
-    // while (true) {
-    //     for (uint32_t value : values) {
-    //         printf("Here's a fun value =D %d", value);
-    //     }
-    // }
+  }
+
+  // printf("%d:\t%d\n", lightSensor2.readRegister(TSL2572::C0DATA), 2);
+  // printf("%d:\t%d\n", lightSensor3.readRegister(TSL2572::C0DATA), 3);
+  // printf("%d:\t%d\n", lightSensor4.readRegister(TSL2572::C0DATA), 4);
+
+  // for (i = 4; i < 8; i++) {
+  //     //mux.writeRegister(0b00010000 << (i - 4));
+  //     printf("%d:\t%d\n", lightSensor.readRegister(TSL2572::C0DATA), i - 4);
+  // }
+  // auto i2c_lock = _i2c.lock();
+  // uint32_t values[4];
+  // uint8_t channel = 2;
+  // for (int i = 0; i < 4; i++)
+  // {
+  //     //channel selector
+  //     bool error = writeByte(226, 0, channel);
+
+  // // while(true) {
+  // //     printf("lux: %ld\n", lightSensor.calculateLux(1));
+  // // }
+  // return 0;
+
+  //     channel << 1;
+  // }
+  // while (true) {
+  //     for (uint32_t value : values) {
+  //         printf("Here's a fun value =D %d", value);
+  //     }
+  // }
 }
