f281676b001bda3ae87ef58026ec84d6e27e7f81
Kasra Alexander Sohrab <67869617+kasohrab@users.noreply.github.com>
2022-10-07 11:17:00 -0400

    add and update comments

--- control/include/robocup.hpp
+++ control/include/robocup.hpp
@@ -2,15 +2,22 @@
 #define CONTROL_ROBOCUP_HPP
 
 /**
- * @headerfile global configuration values for robocup firmware are defined here
+ * @file robocup.hpp
+ * @brief global configuration values for robocup firmware are defined here
  */
 
 /**
- * @def  Whether or not we are using an RTOS.
+ * @def USING_RTOS
+ * @brief  Whether or not we are using an RTOS.
  *       This affects how we should be delaying in some places
  */
 #define USING_RTOS
 
+/**
+ * @def NORMAL_BASESTATION_PORT
+ * @brief Comment this out when running with an alternate basestation port
+ *        (used for two team control)
+ */
 #define NORMAL_BASESTATION_PORT
 
 #endif  // CONTROL_ROBOCUP_HPP
