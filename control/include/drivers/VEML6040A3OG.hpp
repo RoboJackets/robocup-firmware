e00485b04d22aefca40987a29cf26ae9f7c78d5d
Evan Merritt <emerritt8@gatech.edu>
2022-11-01 20:16:40 -0400

    Basis header outline

--- control/include/drivers/VEML6040A3OG.hpp
+++ control/include/drivers/VEML6040A3OG.hpp
@@ -1,1 +1,14 @@
-std::cout<<"Hello world!">>
+#pragma once
+
+#include <memory>
+#include "I2C.hpp"
+#include "LockedStruct.hpp"
+
+class VEML6040A3OG {
+    private:
+    LockedStruct<I2C>& _i2c;
+    int _i2cAddress;  // physical I2C address
+
+    // Cached copies of the register values
+    uint16_t _cachedGPIO, _cachedIODIR, _cachedGPPU, _cachedIPOL;
+};
