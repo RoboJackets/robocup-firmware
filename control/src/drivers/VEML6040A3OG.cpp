c8450401e84c1c4ec7dbc88dba207072fc919096
Evan Merritt <emerritt8@gatech.edu>
2022-11-01 20:31:00 -0400

    Initial work on constructor

---
+++ control/src/drivers/VEML6040A3OG.cpp
@@ -0,0 +1,6 @@
+#include "LockedStruct.hpp"
+#include "drivers/VEML6040A3OG.hpp"
+
+VEML6040A3OG::VEML6040A3OG(LockedStruct<I2C>& sharedI2C, int i2cAddress)
+    : _i2c(sharedI2C), _i2cAddress(i2cAddress) {
+}
