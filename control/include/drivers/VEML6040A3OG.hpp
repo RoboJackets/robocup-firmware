2522b34303e230709f3c7877d425c7063fcf5dda
cchoi95 <gundamguy5001@gmail.com>
2022-11-13 19:20:22 -0500

    change VEML6040A3OG

--- control/include/drivers/VEML6040A3OG.hpp
+++ control/include/drivers/VEML6040A3OG.hpp
@@ -8,6 +8,7 @@ class VEML6040A3OG {
     private:
     LockedStruct<I2C>& _i2c;
     int _i2cAddress;  // physical I2C address
+    
 
     // Cached copies of the register values
     uint16_t _cachedGPIO, _cachedIODIR, _cachedGPPU, _cachedIPOL;
