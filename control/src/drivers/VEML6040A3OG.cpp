0b7454ddb64331565d79530e99a50aacc75a11fa
Evan Merritt <emerritt8@gatech.edu>
2022-11-20 19:31:28 -0500

    Made consistent with header file

--- control/src/drivers/VEML6040A3OG.cpp
+++ control/src/drivers/VEML6040A3OG.cpp
@@ -1,15 +1,15 @@
 #include "LockedStruct.hpp"
 #include "drivers/VEML6040A3OG.hpp"
-VEML6040A3OG::VEML6040A3OG(int32_t,sensorID, I2C &sharedI2C)
-    :_i2c(sharedI2C)
+VEML6040A3OG::VEML6040A3OG(int32_t sensorID, I2C &sharedI2C)
+    :_VEML6040A3OGSensorID(sensorID), _i2c(sharedI2C)
 {
     this->_VEML6040A3OGSensorID=sensorID;
 }
-void VEML6040A306::writeRegister(unit8_t data)
+void VEML6040A3OG::writeRegister(uint8_t data)
 {
     _i2c.transmit(0xE2h,data);
 }
 uint8_t VEML6040A3OG::readRegister()
 {
-    uint8_t buffer
+    uint8_t buffer;
 }
