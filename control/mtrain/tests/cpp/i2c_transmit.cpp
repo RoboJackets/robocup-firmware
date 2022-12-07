fc5a045ab83a97c91d6cf6541dadc5d57084d463
bperez40 <bperez40@gatech.edu>
2022-12-06 19:54:05 -0500

    Changed test

--- control/mtrain/tests/cpp/i2c_transmit.cpp
+++ control/mtrain/tests/cpp/i2c_transmit.cpp
@@ -4,11 +4,10 @@
 /* I2C test using w/ blocking transmit and receive functions */
 int main() {
     I2C i2c1(I2CBus1);
-    fflush(stdout);
     while (true) {
-        HAL_StatusTypeDef ret = i2c1.transmit(0x08, 0b00000010);
+        HAL_StatusTypeDef ret = i2c1.transmit(0x04, 2);
         HAL_Delay(500);
-        printf("%X", ret);
+        printf("Transmitting values");
         printf("\r\n");
         HAL_Delay(500);
     }
