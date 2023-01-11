c6109e699cd44b4ea64c526dd6e2c5bb1a546f02
Kasra Alexander Sohrab <67869617+kasohrab@users.noreply.github.com>
2023-01-10 19:59:42 -0500

    small adjustments to improve robot response (#171)
    * small adjustments
    
    * improved response
    
    * Automatic style
    
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    
    Signed-off-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: kasohrab <kasohrab@users.noreply.github.com>

--- control/src/robocup/modules/RadioModule.cpp
+++ control/src/robocup/modules/RadioModule.cpp
@@ -130,11 +130,13 @@ void RadioModule::receive() {
         // Try read
         // Clear buffer of old packets such that we can get the latest packet
         // If you don't do this there is a significant lag of 300ms or more
-        vTaskSuspendAll();
-        while (link.receive(received_kicker_command, received_motion_command))
-            ;
+        auto cont = true;
+        while (cont) {
+            vTaskSuspendAll();
+            cont = link.receive(received_kicker_command, received_motion_command);
+            xTaskResumeAll();
+        }
         // printf("RadioRaw: %lu\r\n", HAL_GetTick());
-        xTaskResumeAll();
 
         if (received_motion_command.isValid) {
             motionCommand.lock().value() = received_motion_command;
