e976a4e1ff37592c857a34a546f04b740f0203d5
kasohrab <kasohrab@users.noreply.github.com>
2023-04-22 16:39:56 +0000

    Automatic style
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- control/mtrain/API/c/Src/timer.c
+++ control/mtrain/API/c/Src/timer.c
@@ -1,8 +1,7 @@
 #include "timer.h"
 TIM_HandleTypeDef htim;
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htimer)
-{
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htimer) {
     if (htimer->Instance == TIM3) {
         // Timer interrupt code here
         NVIC_SystemReset();
@@ -13,7 +12,7 @@ void MX_TIM6_Init() {
     htim.Instance = TIM3;
     htim.Init.Prescaler = 0;
     htim.Init.CounterMode = TIM_COUNTERMODE_UP;
-    htim.Init.Period = 80000000; // Set timer period to 8 seconds
+    htim.Init.Period = 80000000;  // Set timer period to 8 seconds
     htim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
     __HAL_TIM_ENABLE(&htim);
     /* Enable the TIM6 interrupt. This must execute at the lowest interrupt priority. */
@@ -24,22 +23,16 @@ void MX_TIM6_Init() {
     }
 }
 
-void Start_TIM6() {
-    HAL_TIM_Base_Start_IT(&htim);
-}
+void Start_TIM6() { HAL_TIM_Base_Start_IT(&htim); }
 
-void Stop_TIM6() {
-    HAL_TIM_Base_Stop_IT(&htim);
-}
+void Stop_TIM6() { HAL_TIM_Base_Stop_IT(&htim); }
 
 /**
-  * @brief TIM3 Initialization Function
-  * @param None
-  * @retval None
-  */
-void MX_TIM3_Init(void)
-{
-
+ * @brief TIM3 Initialization Function
+ * @param None
+ * @retval None
+ */
+void MX_TIM3_Init(void) {
     /* USER CODE BEGIN TIM3_Init 0 */
 
     /* USER CODE END TIM3_Init 0 */
@@ -57,20 +50,17 @@ void MX_TIM3_Init(void)
     htim.Init.Period = 0x1;
     htim.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
     htim.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
-    if (HAL_TIM_Base_Init(&htim) == HAL_OK)
-    {
+    if (HAL_TIM_Base_Init(&htim) == HAL_OK) {
         // idk
         __HAL_TIM_ENABLE(&htim);
     }
     sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
-    if (HAL_TIM_ConfigClockSource(&htim, &sClockSourceConfig) != HAL_OK)
-    {
+    if (HAL_TIM_ConfigClockSource(&htim, &sClockSourceConfig) != HAL_OK) {
         // idk
     }
     sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
     sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
-    if (HAL_TIMEx_MasterConfigSynchronization(&htim, &sMasterConfig) != HAL_OK)
-    {
+    if (HAL_TIMEx_MasterConfigSynchronization(&htim, &sMasterConfig) != HAL_OK) {
         // idk
     }
     /* USER CODE BEGIN TIM3_Init 2 */
@@ -81,14 +71,12 @@ void MX_TIM3_Init(void)
     }
 
     /* USER CODE END TIM3_Init 2 */
-
 }
 
 /**
-  * @brief This function handles TIM3 global interrupt.
-  */
-void TIM3_IRQHandler(void)
-{
+ * @brief This function handles TIM3 global interrupt.
+ */
+void TIM3_IRQHandler(void) {
     /* USER CODE BEGIN TIM3_IRQn 0 */
 
     /* USER CODE END TIM3_IRQn 0 */
